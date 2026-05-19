97670d29cdf90795cf20fb05e978ecfa6e1641f2
Haruki Oshiro <68176059+Harui-i@users.noreply.github.com>
2026-05-19 18:05:52 +0900

    problemsを変更

--- problems/testdir/A/testdir_A.cpp
+++ problems/testdir/A/testdir_A.cpp
@@ -21,7 +21,6 @@
 #include <utility>
 #include <vector>
 
-#include "lib/debug.hpp"
 using namespace std;
 
 using ll = long long;
@@ -29,8 +28,14 @@ using ll = long long;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n_input; cin >> n_input;
+  vector<int> a_input(n_input);
+  for (int i = 0; i < n_input; i++) cin >> a_input[i];
 
-  cout << "Hello, World!" << endl;
+  cout << "Hello,world!" << endl;
+  cout << "n_input: " << n_input << endl;
+  for (int a : a_input) cout << a << " ";
+  cout << endl;
 
   return 0;
 }
