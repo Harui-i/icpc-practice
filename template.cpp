c9478cc2b51f5451f22568508beeac891241c2ae
Haruki Oshiro <68176059+Harui-i@users.noreply.github.com>
2026-06-16 08:30:18 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -24,6 +24,12 @@
 using namespace std;
 
 using ll = long long;
+template <class T> inline bool chmax(T& a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <class T> inline bool chmin(T& a, const T& b) { if (b < a) { a = b; return true; } return false; }
+const int INTINF = 1000001000;
+const int INTMAX = 2147483647;
+const ll LLMAX = 9223372036854775807;
+const ll LLINF = 1000000000000000000;
 
 int main() {
   ios::sync_with_stdio(false);
